4d143e82a1e8a6ca0c300d332c930a4a4ae00347
admin <yito@mikilab.doshisha.ac.jp>
2021-10-20 21:00:57 +0900

    ログインページの追加に先立ちレイアウトコンポーネントの配置を変更

--- webfront/src/view/App.tsx
+++ webfront/src/view/App.tsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Service from './service/Service';
-import Layout from './layout/Layout';
+import Header from './layout/Header';
+import SideBar from './layout/SideBar';
+import BackgroundSkin from './layout/BackgroundSkin';
 
 function App() {
+  const [open, setOpen] = useState<boolean>(true);
+
   return (
+    <>
+    <Header open={open} setOpen={setOpen}/>
     <Router>
       <Switch>
         <Route exact path="/">
           Top Page
         </Route>
+        <Route path="/login">
+          Login Page
+        </Route>
         <Route path="/service">
-          <Layout />
+          <SideBar open={open}/>
+          <BackgroundSkin />
           <Service />
         </Route>
       </Switch>
     </Router>
+    </>
   );
 }
 
